bd4f2f8373752bb9d0b2347ffb95b09cbac16d0b
blainedegannes <blaine@entreelife.com>
2011-05-07 17:05:58 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreEntree::Application.config.secret_token = 'f8575cbd01ae32bd4423067743bf0077cf57a93d9cc3d68bb681a0f0bebad828c081bb0df56b1d65644079d40cbf53ce7bc83a3d1e876a3c193eb154a15b991c'
